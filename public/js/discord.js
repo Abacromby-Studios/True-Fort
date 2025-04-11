3b72b1d1b1503aec0e25976add1074f1a63d0739
Abacromby-Studios <abacromby1739@Gmail.com>
2025-04-11 14:00:40 -0400

    Update discord.js

--- public/js/discord.js
+++ public/js/discord.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+  const discordFrame = document.getElementById('discord-widget');
+  if (discordFrame) {
+    discordFrame.src = 'https://discord.com/widget?id=be1351345510199464007&theme=dark';
+  }
+});
