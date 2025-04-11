060bf70084c7b3c3a9938a4c153114d9d60a1230
Abacromby-Studios <abacromby1739@Gmail.com>
2025-04-11 15:00:12 -0400

    Update email.js

--- backend/email.js
+++ backend/email.js
@@ -1,1 +1,22 @@
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+const sendMail = async (email, department, subject, message) => {
+    const mailOptions = {
+        from: process.env.EMAIL_USERNAME,
+        to: process.env.EMAIL_USERNAME,
+        subject: `[${department.toUpperCase()}] ${subject}`,
+        text: `From: ${email}\n\n${message}`
+    };
+
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports = { sendMail };
