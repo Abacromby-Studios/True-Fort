ac9d928afba9b9f1561b10384a2e42b89bfe5f0e
Abacromby-Studios <abacromby1739@Gmail.com>
2025-04-11 13:50:13 -0400

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+  const discordFrame = document.getElementById('discord-widget');
+  if (discordFrame) {
+    discordFrame.src = 'https://discord.com/widget?id=YOUR_SERVER_ID&theme=dark';
+  }
+});
