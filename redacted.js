869de00b1afe899f30e59b973ea0201890f828ac
Abacromby-Studios <abacromby1739@Gmail.com>
2025-04-12 06:19:47 -0400

    Update redacted.js

--- redacted.js
+++ redacted.js
@@ -1,4 +1,4 @@
-const scrambledParts = ['QWw=', 'cGhh', 'bGxvI', 'bGFi', 'ZCBDa', 'HR5I', 'gNNTI', '1NzY=', '1NTg=', '5Mw=='];
+const scrambledParts = ['QWw=', 'cGhh', 'bGxvI', 'bGxi', 'ZCBDa', 'HR5I', 'gNNTI', '1NzY=', '1NTg=', '5Mw=='];
 
 export function decodePassword() {
   const unsort = [2, 3, 4, 0, 1, 6, 5, 8, 9, 7];
