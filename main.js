b156cb68bf7971ca05f769f5348e634e02b7b181
Abacromby-Studios <abacromby1739@Gmail.com>
2025-04-12 17:50:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-// main.js - general site-wide JavaScript - needs coded for aby abd all functionality needed gor pages that require additional functionality, code this file.
+// main.js - general site-wide JavaScript - needs coded for aby abd all functionality needed for pages that require additional functionality, code this file.
