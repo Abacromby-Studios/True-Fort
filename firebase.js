b11950641c2724b7b512e6b75722158909682e8d
Abacromby-Studios <abacromby1739@Gmail.com>
2025-04-14 23:39:56 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,3 +9,23 @@ const firebaseConfig = {
   messagingSenderId: "750729345906",
   appId: "1:750729345906:web:7ab18054f7be68f7327beb"
 };
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+export const requestPermission = async () => {
+  try {
+    const token = await getToken(messaging, {
+      vapidKey: process.env.VAPID_PUBLIC_KEY
+    });
+    if (token) {
+      console.log('Notification token:', token);
+    }
+  } catch (err) {
+    console.error('Failed to get token:', err);
+  }
+};
+
+onMessage(messaging, payload => {
+  console.log('Message received:', payload);
+});
