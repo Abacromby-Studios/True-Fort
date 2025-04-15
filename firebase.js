39db4253bcf7d8adc84db2797078d095fa2a70c5
Abacromby-Studios <abacromby1739@Gmail.com>
2025-04-14 23:38:13 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,11 @@
-// firebase.js - Firebase push notification setup - Needs coded
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_NKYwvwLg2KQOB--RX05rimEiEkcSpho",
+  authDomain: "true-fort.firebaseapp.com",
+  projectId: "true-fort",
+  storageBucket: "true-fort.appspot.com",
+  messagingSenderId: "750729345906",
+  appId: "1:750729345906:web:7ab18054f7be68f7327beb"
+};
